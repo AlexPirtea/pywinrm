6d73824573057847a337ee83a8bb82ff570cdde2
AlexPirtea <mrshadowz@hotmail.com>
2018-06-06 17:29:39 +0300

    Update transport.py

--- winrm/transport.py
+++ winrm/transport.py
@@ -239,7 +239,7 @@ class Transport(object):
                     response = self.session.send(prepared_request, timeout=self.read_timeout_sec)
                     break
                 except requests.exceptions.ConnectionError as e:
-                    if attempt == 4  or 'connection refused' not in str(e).lower() or 'connection reset' not in str(e).lower() or 'connection aborted' not in str(e).lower():
+                    if attempt == 4  or ('connection refused' not in str(e).lower() and 'connection reset' not in str(e).lower() and 'connection aborted' not in str(e).lower()):
                         raise
                     time.sleep(5)
 
